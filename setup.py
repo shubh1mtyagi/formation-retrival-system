9580b394834488eaa173f847c119af473e021b3c
Shubham tyagi <tyagishubham2001@gmail.com>
2025-12-30 13:59:21 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="Generative_AI_Project",
+    version="0.0.0",
+    author="Shubham",
+    author_email="tyagishubham2001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
